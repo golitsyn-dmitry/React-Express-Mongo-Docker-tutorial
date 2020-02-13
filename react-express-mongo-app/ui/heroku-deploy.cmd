0e2554e3c19e2ad90bd3cd7c733e4e1ff7a42b1f
DG <golitsyn.dmitry@gmail.com>
2020-02-13 22:39:40 +0300

    deploy script

--- react-express-mongo-app/ui/heroku-deploy.cmd
+++ react-express-mongo-app/ui/heroku-deploy.cmd
@@ -1,16 +1,7 @@
-## Depoly to heroku
-cd <project_dir>/ui
-docker-compose -up
-
-cd <project_dir>/ui
-docker ps
-
+cd e:
+cd E:\Projects\React-Express-Mongo-Docker-tutorial\react-express-mongo-app\ui
 docker tag react-express-mongo-app_ui registry.heroku.com/afternoon-hollows-45691/web
-
 docker push registry.heroku.com/afternoon-hollows-45691/web
-
 heroku container:release web --app=afternoon-hollows-45691
-
 heroku dyno:scale --app=afternoon-hollows-45691
-
 heroku logs --app=afternoon-hollows-45691
